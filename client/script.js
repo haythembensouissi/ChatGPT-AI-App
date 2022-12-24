d5bbd32749cdf8da85a45946260ec93ac3d94a41
haythembensouissi <haythembensouissi2@gmail.com>
2022-12-24 11:43:49 +0100

    server link added

--- client/script.js
+++ client/script.js
@@ -57,7 +57,7 @@ const handleSubmit= async (e)=>{
     chatContainer.scrollTop=chatContainer.scrollHeight
   const  messageDiv =document.getElementById(uniqueId)
   loader(messageDiv)
-const response=await fetch('http://localhost:5000',{
+const response=await fetch('https://chatgpt-ms8i.onrender.com',{
     method:'POST',
     headers:{
         'Content-Type': 'application/json'
