7756f7abd46d3de1aeb5b7773f156432cc18f97d
haythembensouissi <haythembensouissi2@gmail.com>
2024-08-14 15:53:11 +0100

    api key

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.get("/",async (req,res)=>{
     res.status(200).send({
-        message:"hello from Codex"
+        message:"hello from chatgpt"
     })
 })
 app.post("/",async (req,res)=>{
